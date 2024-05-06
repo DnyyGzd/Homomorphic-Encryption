b35185ce237506326b210143ada60cce71d96acb
Ignazio Daniele Di Blasi <127403997+DnyyGzd@users.noreply.github.com>
2024-05-06 17:59:23 +0200

    Add files via upload

--- setup.bash
+++ setup.bash
@@ -2,13 +2,18 @@
 
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean
 
-sudo apt install -y build-essential cmake curl git tar unzip zip zstd
+sudo apt install -y curl git tar unzip zip zstd \
+                    build-essential clang cmake \
+                    libboost-all-dev libprotobuf-dev protobuf-compiler
+
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 
 cd ~/Documents/
 wget https://github.com/microsoft/SEAL/archive/refs/tags/v4.1.1.zip
-unzip v4.1.1.zip
+unzip v4.1.1.zip -d SEAL
 
-cd ~/Documents/SEAL-4.1.1/
+cd ~/Documents/SEAL/
 cmake -S . -B build -DSEAL_BUILD_EXAMPLES=ON -DSEAL_BUILD_TESTS=ON -DSEAL_BUILD_BENCH=ON -DSEAL_THROW_ON_TRANSPARENT_CIPHERTEXT=OFF
 cmake --build build
 sudo cmake --install build
