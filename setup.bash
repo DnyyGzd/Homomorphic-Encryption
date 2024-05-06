6f67209e4659ec1199e600381d783515fd8f6f14
Ignazio Daniele Di Blasi <127403997+DnyyGzd@users.noreply.github.com>
2024-05-06 18:30:52 +0200

    Add files via upload

--- setup.bash
+++ setup.bash
@@ -6,8 +6,6 @@ sudo apt install -y curl git python3-pip tar unzip zip zstd \
                     build-essential clang cmake \
                     libboost-all-dev libprotobuf-dev protobuf-compiler
 
-pip install numpy
-
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 
@@ -27,6 +25,7 @@ git submodule update --init
 sed -i -e 's/find_package(SEAL 3.6 REQUIRED)/#find_package(SEAL 3.6 REQUIRED)\nfind_package(SEAL 4.1 REQUIRED)/' CMakeLists.txt
 cmake .
 make -j
+python3 -m pip install numpy
 python3 -m pip install -e python/
 python3 python/setup.py bdist_wheel --dist-dir='.'
 python3 tests/all.py
